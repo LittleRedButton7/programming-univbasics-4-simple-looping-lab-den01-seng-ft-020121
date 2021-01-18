c07aea9bcd12d22d4904835cb2aa471d4fdf5828
Marissa Nolan <ritz1484@cs.com>
2021-01-18 19:06:08 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,4 +2,10 @@
 def loop_message_five_times (message_string)
   counter = 5
   
-  while counter
+  while counter > 0
+    puts message_string
+    counter -= 1
+  end
+end
+
+loop_message_five_times("Hello world!")
